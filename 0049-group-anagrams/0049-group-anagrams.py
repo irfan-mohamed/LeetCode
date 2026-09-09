fe6cf2aff212b40642c02492c8c9e541efce438c
IRFAN MOHAMED <114469029+irfan-mohamed@users.noreply.github.com>
2026-09-09 11:38:24 +0530

    Time: 7 ms (98.28%), Space: 22.1 MB (64.6%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,17 +1,5 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        # list1 = []
-        # list3 = []
-        # for i in range(len(strs)):
-        #     if strs[i] in list3 :
-        #         continue
-        #     list2 = [strs[i]]
-        #     for j in range(i+1, len(strs)):
-        #         if (sorted(list(strs[i])) == sorted(list(strs[j]))):
-        #             list2.append(strs[j])
-        #             list3.append(strs[j])
-        #     list1.append(list2)
-        # return list1
         hashmap = dict()
         for string in strs:
             key = "".join(sorted(string))
